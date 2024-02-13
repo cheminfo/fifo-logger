f6b2e1ba90a7199233ddd2f7537bc3fc49c37eba
Luc Patiny <luc@patiny.com>
2024-02-13 10:42:17 +0100

    chore: fix prettier

--- src/CustomEvent.ts
+++ src/CustomEvent.ts
@@ -1,5 +1,3 @@
-
-
 // compatibility for CustomEvent in Node 16 and 18
 // todo should be removed in 2026 ...
 // https://stackoverflow.com/questions/69791262/how-do-i-send-a-customevent-via-a-node-16-eventtarget
